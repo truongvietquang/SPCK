15a46d9322d3e15093a1b49bbdbe9bb6b9e01732
quang <vietquanglop72@gmail.com>
2024-07-14 16:00:17 +0700

    day 4

---
+++ js/smallFeature.js
@@ -0,0 +1,6 @@
+//send message
+const chatBottomSend = document.querySelector(".chat-bottom-send")
+const sendButton = document.querySelector(".send")
+sendButton.addEventListener("click",() => {
+    
+})
